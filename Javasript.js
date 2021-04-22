5463f56dfc057fd9e6517ea1d55c5e18cc357718
nexus0208 <christiandelacruz0208@gmail.com>
2021-04-22 23:35:12 +0800

    finish exercise, success!

--- Javasript.js
+++ Javasript.js
@@ -4,9 +4,9 @@ function Book(title, author, pages, read) {
   this.pages = pages
   this.read = read
   this.info = function() {
-    console.log(name + "by" + author, pages + "pages", read)
+    console.log(title + " by " + author + ", " +  pages + " pages" +", " + read)
   }
 }
 
-const theHobbit = new Book("The Hobbit by J.R.R","J.R.R. Tolkien", 295," not read yet")
-theHobbit.info()
+const theHobbit = new Book("The Hobbit","J.R.R. Tolkien", 295," not read yet")
+theHobbit.info() //The Hobbit by J.R.R. Tolkien, 295 pages,  not read yet
