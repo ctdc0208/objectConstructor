7942c2795b61c500dd26f7c3c7b49934af8e728c
nexus0208 <christiandelacruz0208@gmail.com>
2021-04-22 23:30:13 +0800

    Add info

--- Javasript.js
+++ Javasript.js
@@ -4,6 +4,9 @@ function Book(title, author, pages, read) {
   this.pages = pages
   this.read = read
   this.info = function() {
-    console.log(name, author, pages + "pages", read)
+    console.log(name + "by" + author, pages + "pages", read)
   }
 }
+
+const theHobbit = new Book("The Hobbit by J.R.R","J.R.R. Tolkien", 295," not read yet")
+theHobbit.info()
